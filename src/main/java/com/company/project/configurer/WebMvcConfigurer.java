fd1d1a849e82a8e3c459134de8f394b7286e1480
xiongzhao <zhao.xiong@ivydad.com>
2018-03-22 18:41:09 +0800

    优化

--- src/main/java/com/company/project/configurer/WebMvcConfigurer.java
+++ src/main/java/com/company/project/configurer/WebMvcConfigurer.java
@@ -41,31 +41,47 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
 
     private final Logger logger = LoggerFactory.getLogger(WebMvcConfigurer.class);
-    @Value("${spring.profiles.active}")
-    private String env;//当前激活的配置文件
 
-    //使用阿里 FastJson 作为JSON MessageConverter
+    /**
+     * 当前环境变量
+     */
+    @Value("${spring.profiles.active}")
+    private String env;
+
+    /**
+     * 使用阿里 FastJson 作为JSON MessageConverter
+     * @param converters
+     */
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
         FastJsonConfig config = new FastJsonConfig();
         config.setDateFormat("yyyy-MM-dd HH:mm:ss");
-        config.setSerializerFeatures(SerializerFeature.WriteMapNullValue,//保留空的字段
-                SerializerFeature.WriteNullStringAsEmpty,//String null -> ""
-                SerializerFeature.WriteNullNumberAsZero);//Number null -> 0
+        config.setSerializerFeatures(
+                // 保留空的字段
+                SerializerFeature.WriteMapNullValue,
+                // String null -> ""
+                SerializerFeature.WriteNullStringAsEmpty,
+                // Number null -> 0
+                SerializerFeature.WriteNullNumberAsZero);
         converter.setFastJsonConfig(config);
         converter.setDefaultCharset(Charset.forName("UTF-8"));
         converters.add(converter);
     }
 
 
-    //统一异常处理
+    /**
+     * 统一异常处理
+     * @param exceptionResolvers
+     */
     @Override
     public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
         exceptionResolvers.add(new HandlerExceptionResolver() {
+            @Override
             public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception e) {
                 Result result = new Result();
-                if (e instanceof ServiceException) {//业务失败的异常，如“账号或密码错误”
+                //业务失败的异常，如“账号或密码错误”
+                if (e instanceof ServiceException) {
                     result.setCode(ResultCode.FAIL).setMessage(e.getMessage());
                     logger.info(e.getMessage());
                 } else if (e instanceof NoHandlerFoundException) {
@@ -94,17 +110,24 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
         });
     }
 
-    //解决跨域问题
+    /**
+     * 解决跨域问题
+     * @param registry
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         //registry.addMapping("/**");
     }
 
-    //添加拦截器
+    /**
+     * 添加拦截器
+     * @param registry
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //接口签名认证拦截器，该签名认证比较简单，实际项目中可以使用Json Web Token或其他更好的方式替代。
-        if (!"dev".equals(env)) { //开发环境忽略签名认证
+        //开发环境忽略签名认证
+        if (!"dev".equals(env)) {
             registry.addInterceptor(new HandlerInterceptorAdapter() {
                 @Override
                 public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -144,25 +167,31 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
      * 3. 混合密钥（secret）进行md5获得签名，与请求的签名进行比较
      */
     private boolean validateSign(HttpServletRequest request) {
-        String requestSign = request.getParameter("sign");//获得请求签名，如sign=19e907700db7ad91318424a97c54ed57
+        //获得请求签名，如sign=19e907700db7ad91318424a97c54ed57
+        String requestSign = request.getParameter("sign");
         if (StringUtils.isEmpty(requestSign)) {
             return false;
         }
         List<String> keys = new ArrayList<String>(request.getParameterMap().keySet());
-        keys.remove("sign");//排除sign参数
-        Collections.sort(keys);//排序
+        //排除sign参数
+        keys.remove("sign");
+        //排序
+        Collections.sort(keys);
 
         StringBuilder sb = new StringBuilder();
         for (String key : keys) {
-            sb.append(key).append("=").append(request.getParameter(key)).append("&");//拼接字符串
+            //拼接字符串
+            sb.append(key).append("=").append(request.getParameter(key)).append("&");
         }
         String linkString = sb.toString();
-        linkString = StringUtils.substring(linkString, 0, linkString.length() - 1);//去除最后一个'&'
+        //去除最后一个'&'
+        linkString = StringUtils.substring(linkString, 0, linkString.length() - 1);
+        //密钥，自己修改
+        String secret = "Potato";
+        //混合密钥md5
+        String sign = DigestUtils.md5Hex(linkString + secret);
 
-        String secret = "Potato";//密钥，自己修改
-        String sign = DigestUtils.md5Hex(linkString + secret);//混合密钥md5
-
-        return StringUtils.equals(sign, requestSign);//比较
+        return StringUtils.equals(sign, requestSign);
     }
 
     private String getIpAddress(HttpServletRequest request) {
